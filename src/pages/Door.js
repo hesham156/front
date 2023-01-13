62bf5c6207f74a784c81ccf2bf82134370dab6a4
hesham156 <h456ad@gmail.com>
2023-01-13 17:35:33 +0200

    5:35

--- src/pages/Door.js
+++ src/pages/Door.js
@@ -2,11 +2,35 @@ import React, { useEffect, useState } from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
 import Login from './Login'
 import Register from './Register'
+import GoogleLogin from '@leecheuk/react-google-login';
+import {FaFacebook} from 'react-icons/fa'
+import FacebookLogin from 'react-facebook-login';
+import { toast } from 'react-toastify';
+import { loadGapiInsideDOM } from "gapi-script";
 
 const Door = () => {
   const[active,setActive]=useState('login')
-
+  const redirect = ()=>{   
+    window.location.href = '/'
+  }   
+const addToStorage=(name,email,password,image)=>{
+    localStorage.setItem('user',JSON.stringify({'name':name,'email':email,'password':password,'image':image}))
+    toast.success("Success")
+    redirect()
+  }
+  const responseFacebook = (response) => {
+    addToStorage(response?.name,response?.email,response?.userID,response?.picture.data.url) 
+  }
+   
+  const responseGoogle = (response) => {
+    addToStorage(response?.profileObj.name,response?.profileObj.email,response?.profileObj.googleId,response?.profileObj.imageUrl) 
+  
+  
+  }
   useEffect(() => {
+    (async () => {
+      await loadGapiInsideDOM();
+    })();
   setActive(window.location.pathname.replace('/door/',''))
   }, []);
   return (
@@ -19,9 +43,28 @@ const Door = () => {
         </div>
         <div className='doorBootom'>
                     <Routes>
-                        <Route path='/login' element={<Login/>} />
-                        <Route path='/register' element={<Register/>} />
+                        <Route path='/login' element={<Login redirect={redirect} addToStorage={addToStorage}/>} />
+                        <Route path='/register' element={<Register redirect={redirect} addToStorage={addToStorage}/>} />
                     </Routes>
+        </div>
+        <div className='social-login'>
+        <div className='socialLogin'>
+      <GoogleLogin
+    clientId= "377316420785-rkcjm8tut58icdbkenblo56oqqra54re.apps.googleusercontent.com"
+    buttonText=""
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  />
+   <FacebookLogin
+    appId="913562719798311"
+    fields="name,email,picture"
+    icon={<FaFacebook/>}
+    textButton=''
+    // onClick={componentClicked}
+    callback={responseFacebook} />
+  
+    </div>
         </div>
         </div>
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,66 +1,29 @@
 import '../assets/css/login.css'
-import GoogleLogin from '@leecheuk/react-google-login';
-import {FaFacebook} from 'react-icons/fa'
-import { loadGapiInsideDOM } from "gapi-script";
+
 import { useEffect, useState } from 'react';
-import FacebookLogin from 'react-facebook-login';
 import { toast } from 'react-toastify';
 
 
-const Login = () => {
-  const redirect = ()=>{   
-    window.location.href = '/'
-  }
-  
+const Login = ({redirect,addToStorage}) => {
     const [uEmail,setUEmail] = useState()
     const [uPass,setUPass] = useState()
-    const [user,setuser] = useState({})
-    const addToStorage=(name,email,password,image)=>{
-      localStorage.setItem('user',JSON.stringify({'name':name,'email':email,'password':password,'image':image}))
-      toast.success("Success")
-      redirect()
-    }
+
+
 const loginfun = ()=>{
      if(uEmail && uPass){
        if(uEmail.search('@')>=0){
        addToStorage(uEmail,uEmail,uPass,'https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745')
+       redirect()
        }else{toast.error("please inter a valid email")}
      }else{toast.error("please inter all data")}
 }
-const responseFacebook = (response) => {
-  addToStorage(response?.name,response?.email,response?.userID,response?.picture.data.url) 
-}
- 
-const responseGoogle = (response) => {
-  addToStorage(response?.profileObj.name,response?.profileObj.email,response?.profileObj.googleId,response?.profileObj.imageUrl) 
 
 
-}
-useEffect(() => {
-  (async () => {
-    await loadGapiInsideDOM();
-  })();
-});
   return (
   <div className='login w-100 center flex-column'>
     <div className='loginText center flex-column'>
       <h1>Welcome Back</h1>
-      <div className='socialLogin'>
-      <GoogleLogin
-    clientId= "377316420785-rkcjm8tut58icdbkenblo56oqqra54re.apps.googleusercontent.com"
-    buttonText=""
-    onSuccess={responseGoogle}
-    onFailure={responseGoogle}
-    cookiePolicy={'single_host_origin'}
-  />
-   <FacebookLogin
-    appId="913562719798311"
-    fields="name,email,picture"
-    icon={<FaFacebook/>}
-    textButton=''
-    // onClick={componentClicked}
-    callback={responseFacebook} />
-    </div>
+    
     </div>
      <div className='loginForm center flex-column'>
       <label htmlFor="email">Email</label>
