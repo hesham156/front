9f1d35d51de5176ad2a6ec4307e19265a21926b0
hesham156 <h456ad@gmail.com>
2023-01-31 16:45:57 +0200

    NEW

--- src/component/Tasks.js
+++ src/component/Tasks.js
@@ -4,6 +4,7 @@ const Tasks = ({tasks}) => {
   return (
     <div className='colum tasks'>
         <div className='container'>
+            <h4>TASKS</h4>
             {tasks.map((task)=>{
                 return(<div className='task'>
                         <p>{task.taskName}</p>
